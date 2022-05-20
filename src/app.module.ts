8c4b2bd038669fc2dfd6481fed355f94db000b15
rediaR72 <rediaR72@yandex.rum>
2022-05-20 18:05:11 +0500

    Docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,12 @@ import { RolesModule } from './roles/roles.module'
 import { Role } from './roles/roles.model'
 import { UserRoles } from './roles/user_roles.model'
 import { AuthModule } from './auth/auth.module'
+import { PostsModule } from './posts/posts.module'
+import { Post } from './posts/posts.model'
+import { FilesModule } from './files/files.module'
+
+import { ServeStaticModule } from '@nestjs/serve-static'
+import * as path from 'path'
 
 @Module({
   controllers: [],
@@ -15,6 +21,11 @@ import { AuthModule } from './auth/auth.module'
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`
     }),
+
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static')
+    }),
+
     SequelizeModule.forRoot({
       dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
@@ -22,12 +33,15 @@ import { AuthModule } from './auth/auth.module'
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
-      models: [User, Role, UserRoles],
+      models: [User, Role, UserRoles, Post],
       autoLoadModels: true
     }),
     UsersModule,
     RolesModule,
-    AuthModule
+    AuthModule,
+    PostsModule,
+    FilesModule
   ]
 })
 export class AppModule {}
+console.log(ServeStaticModule.forRoot)
--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -3,9 +3,11 @@ import {
   BelongsToMany,
   Column,
   DataType,
+  HasMany,
   Model,
   Table
 } from 'sequelize-typescript'
+import { Post } from 'src/posts/posts.model'
 import { Role } from 'src/roles/roles.model'
 import { UserRoles } from 'src/roles/user_roles.model'
 
@@ -44,4 +46,7 @@ export class User extends Model<User, UserCreationAttrs> {
 
   @BelongsToMany(() => Role, () => UserRoles)
   roles: Role[]
+
+  @HasMany(() => Post)
+  posts: Post[]
 }
